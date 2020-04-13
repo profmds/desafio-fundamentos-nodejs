70e7b66404fc3a94fd41c8beeb82ad03e2a613c7
Marcelo dos Santos <profmds@gmail.com>
2020-04-13 20:00:45 -0300

    build: Challenge nodejs fundamentals

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+
+    const income = this.transactions.reduce((result, transaction) => {
+      if (transaction.type === 'income') return result + transaction.value;
+
+      return result;
+    }, 0);
+    const outcome = this.transactions.reduce((result, transaction) => {
+      if (transaction.type === 'outcome') return result + transaction.value;
+
+      return result;
+    }, 0);
+
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    // inicialmente foi criada um DTO para a transferência de objetos no formato desestruturado
+    // um novo objeto de transação é criado e recebe os valores passados pelo DTO no services das transictions
+    const transaction = new Transaction({ title, value, type });
+    // a nova transação é então adicionada ao array de transações
+    this.transactions.push(transaction);
+    // por fim a transação é retornada ao services
+    return transaction;
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,31 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+/* function validateRepositoryId(request, response, next){
+  const { id } = request.params;
+
+  if(!isUuid(id)){
+    return response.status(400).json({ error: 'Invalid repository ID.'});
+  }
+  return next();
+} */
+
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+    const printTransaction = {
+      transactions,
+      balance,
+    };
+
+    return response.json(printTransaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +33,25 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    // recebendo os parâmetros do front
+    const { title, value, type } = request.body;
+
+    // criando o objeto de criação da transaçãod com o conceito de Dependency Invertion
+    // para que sempre a lista de transações abertas e não novas criadas
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    // chamada da execução do servço para que os dados da transação sejam deveras adicionado
+    // no array de transações, para que o principío de cada arquivo realizar apenas seu papel
+    // manipulações de dados serão enviadas aos serviços, porém adicionadas através do repositório
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
